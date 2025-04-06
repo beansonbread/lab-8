453ee98ad7954c08203ca9189f8a4abdae09e96c
atento <96808973+beansonbread@users.noreply.github.com>
2025-04-06 18:50:19 -0400

    part 2: converting object to string and string back to object

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const student = {
 }
 
 console.log(student.name, student.age); // output: ashley 24
+
+//part 2
+const jsonString = JSON.stringify(student); // convert object to JSON string
+console.log(jsonString); // output: {"name":"ashley","age":24,"enrolled":true,"courses":["math","english","science"]}
+
+const jsonObject = JSON.parse(jsonString); // convert JSON string to object
+console.log(jsonObject); 
+
