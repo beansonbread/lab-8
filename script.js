422a2c44165274496436129bee5cb3294d842bb7
atento <96808973+beansonbread@users.noreply.github.com>
2025-04-06 18:49:21 -0400

    added part 1: creating student object

---
+++ script.js
@@ -0,0 +1,9 @@
+//part 1
+const student = {
+    name: "ashley",
+    age: 24,
+    enrolled: true,
+    courses: ["math", "english", "science"],
+}
+
+console.log(student.name, student.age); // output: ashley 24
