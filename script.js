ab390bf48f46de3d00799db7cfd959b81031ced2
atento <96808973+beansonbread@users.noreply.github.com>
2025-04-06 18:51:17 -0400

    part 3: destructuring object and courses array

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ console.log(jsonString); // output: {"name":"ashley","age":24,"enrolled":true,"c
 const jsonObject = JSON.parse(jsonString); // convert JSON string to object
 console.log(jsonObject); 
 
+//part 3
+let {name, age, enrolled, courses} = student; 
+console.log(name); 
+console.log(age);
+console.log(enrolled);
+console.log(courses);
+
+let scores = [ 85, 92, 78, 90 ];
+let [ first , second ] = scores;
+console.log(first);
+console.log(second);
+
